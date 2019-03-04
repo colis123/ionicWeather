f9780e30b1b1c424b831faac0e73e0bdcec2cff6
Pedro Colis <colis321@yahoo.com>
2019-03-03 19:26:45 -0800

    weather service functionality

---
+++ src/app/weather.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from "rxjs/operators";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  apiKey = '3840e7b1a421142d6d0e1c25d9b6d9ee';
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+  globalUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=';
+  
+
+  constructor( private http: Http) { }
+
+  getData(city,country) {
+    return this.http.get(`${this.url}${city},${country}&units=imperial&APPID=${this.apiKey}`).pipe(map(res => res.json()));
+  }
+
+  getGeo(lat, lon) {
+    return this.http.get(`${this.globalUrl}${lat}&lon=${lon}&units=imperial&APPID=${this.apiKey}`).pipe(map(res => res.json()));
+  }
+}
